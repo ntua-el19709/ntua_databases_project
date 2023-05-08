7354323979b60f12c84d8a9e7e9ba8f84497b7fe
ntua-el19709 <el19709@mail.ntua.gr>
2023-05-09 00:05:43 +0300

    set server and 1st endpoint to check connectivity

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,39 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+/* ROUTES and how to import routes */
+const allusers = require("./api/allusers");
+/* end of ROUTES and how to import routes */
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+console.log(`NODE_ENV = |${process.env.NODE_ENV}|`);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-OBSERVATORY-AUTH"
+  );
+  next();
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// /* Routes used by our project */
+const baseurl = "/libraries";
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(baseurl + "/allusers", allusers);
+// /*End of routes used by our project */
+
+// In case of an endpoint does not exist
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Endpoint not found!" });
 });
 
 module.exports = app;
