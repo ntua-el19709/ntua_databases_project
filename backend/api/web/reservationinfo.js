2ffc50ae08fe3368593b2f5a5fb820cd9f6dcddf
ntua-el19709 <el19709@mail.ntua.gr>
2023-05-12 00:03:49 +0300

    endpoint reservationinfo

---
+++ backend/api/web/reservationinfo.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const apiutils = require("../../apiutils");
+const Parser = require("@json2csv/plainjs").Parser;
+const router = express.Router();
+
+router.get("/:resID", async (req, res) => {
+  await apiutils.requestWrapper(
+    true,
+    req,
+    res,
+    "Successful retrieval of reservation!",
+    async (conn) => {
+      const results = await conn.query(
+        `SELECT * FROM reservation,book,users
+            WHERE reservation.reservation_id = ? AND reservation.isbn=book.isbn AND reservation.school_id=book.school_id AND reservation.user_id=users.user_id`,
+        [req.params.resID]
+      );
+
+      let json_q = {
+        resID: results[0].reservation_id,
+        username: results[0].username,
+        book: results[0].title,
+        placed_at: results[0].reservation_datetime,
+      };
+
+      if (req.query.format == "csv") {
+        return new Parser().parse(json_q);
+      } else {
+        return json_q;
+      }
+    }
+  );
+});
+
+module.exports = router;
