64cdcbaa80e169f2cf4ff8c931f589f848ca6d57
ntua-el19709 <el19709@mail.ntua.gr>
2023-05-29 16:24:25 +0300

    return schools which do not have an operator yet at sign up

--- backend/api/web/allschools.js
+++ backend/api/web/allschools.js
@@ -10,7 +10,10 @@ router.get("/", async (req, res) => {
     res,
     "Successful retrieval of all schools!",
     async (conn) => {
-      const ans_list = await conn.query("SELECT * FROM school");
+      const ans_list = await conn.query(
+        `SELECT * FROM school WHERE (school_id) 
+          NOT IN (SELECT (school_id) from operator)`
+      );
 
       json_res = [];
       for (elem of ans_list) {
