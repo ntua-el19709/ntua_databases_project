74ed8dcf8fe8289b06f78d2c3dd6e95c7e31ccf7
ntua-el19709 <el19709@mail.ntua.gr>
2023-05-30 15:37:25 +0300

    query 3.1.4 done

--- backend/api/queries/query314.js
+++ backend/api/queries/query314.js
@@ -3,4 +3,29 @@ const apiutils = require("../../apiutils");
 const Parser = require("@json2csv/plainjs").Parser;
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  await apiutils.requestWrapper(
+    true,
+    req,
+    res,
+    "Query 3.1.4 executed succesfully!",
+    async (conn) => {
+      const ans_list = await conn.query(
+        `SELECT author.author_fullname AS author FROM author, book_author, book
+              WHERE author.author_id=book_author.author_id AND book_author.isbn=book.isbn AND book_author.school_id=book.school_id
+              AND (book.isbn,book.school_id) NOT IN (SELECT rental.isbn,rental.school_id FROM rental)`
+      );
+
+      json_res = [];
+      for (elem of ans_list) {
+        json_res.push(elem.author);
+      }
+
+      return json_res;
+    }
+  );
+});
+
+module.exports = router;
+
 module.exports = router;
--- frontend/src/components/Queries/Query314.jsx
+++ frontend/src/components/Queries/Query314.jsx
@@ -3,11 +3,43 @@ import React, { Component } from "react";
 class Query314 extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      results: [],
+    };
   }
+
+  sendquery = () => {
+    fetch(`http://localhost:9103/libraries/queries/query314`)
+      .then((response) => response.json())
+      .then((obj) => {
+        console.log(obj);
+        this.setState({
+          ...this.state,
+          results: obj.map((result) => {
+            return result;
+          }),
+        });
+      });
+  };
 
   render() {
     console.log("Now at Query314");
-    return <div>Query 3.1.4</div>;
+    return (
+      <div>
+        <h2>Query 3.1.4</h2>
+        <p>
+          Βρείτε τους συγγραφείς των οποίων κανένα βιβλίο δεν έχει τύχει
+          δανεισμού.
+        </p>
+        <button onClick={this.sendquery}>Send Query</button>
+        <h3>Αποτελέσματα:</h3>
+        <ul>
+          {this.state.results.map((result) => (
+            <li key={result}>{result}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
